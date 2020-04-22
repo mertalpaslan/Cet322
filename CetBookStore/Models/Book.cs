ebe04cd2282cf588045acf8089b8e8353ce91171
Mert Alpaslan <alpaslanmert@yahoo.com>
2020-04-22 15:36:30 +0300

    Home Work 2

--- CetBookStore/Models/Book.cs
+++ CetBookStore/Models/Book.cs
@@ -11,24 +11,37 @@ namespace CetBookStore.Models
     {
         public int Id { get; set; }
 
-        [StringLength(512, MinimumLength =3)]
-        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Kitap Adı alanı 3 ile 100 karakter arasında olmalıdır")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
         [Display(Name ="Kitap Adı")]
         public string Title { get; set; } // nvarchar(512), not nullable
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        [Display(Name = "Sayfa Sayısı")]
         public int? PageCount { get; set; }
-
+        [Display(Name = "Yazar")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
         public string Authors { get; set; }
-        public string Description  { get; set; }
-        
-        public Decimal Price { get; set; }
 
+        [Display(Name = "Açıklama")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public string Description  { get; set; }
+        [Display(Name = "Fiyat")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public Decimal Price { get; set; }
+        [Display(Name = "Yıl")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
         public int PressYear { get; set; }
+        [Display(Name = "Stok Sayısı")]
         public int StockCount { get; set; }
 
         
         public int CategoryId { get; set; }
         //[ForeignKey("CategoryId")]
-        public  Category Category { get; set; }
+        [Display(Name = "Kategori")]
+
+        public Category Category { get; set; }
+        [Display(Name = "Oluşturulma Tarihi")]
+
         public DateTime CreatedDate { get; set; }
         public virtual List<Comment> Comments { get; set; }
         public Book()
--- CetBookStore/Models/Comment.cs
+++ CetBookStore/Models/Comment.cs
@@ -2,19 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace CetBookStore.Models
 {
     public class Comment
     {
         public int Id{ get; set; }
-        public string Title { get; set; }
-        public string Detail { get; set; }
-        
-        public int? Rating { get; set; }
-        public DateTime CreatedDate { get; set; }
 
+        [Display(Name = "Başlık")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Başlık Alanı 3 ile 100 karakter arasında olmalıdır")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+
+
+        public string Title { get; set; }
+        [Display(Name = "Detaylar")]
+        [StringLength(500, MinimumLength = 3, ErrorMessage = "Detay alanı 3 ile 500 karakter arasında olmalıdır")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+
+        public string Detail { get; set; }
+        [Display(Name = "Puan")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+
+        public int? Rating { get; set; }
+        [Display(Name = "Oluşturulma Tarihi")]
+        public DateTime CreatedDate { get; set; }
+        [Display(Name = "Yorumun Yapıldığı Kitap")]
         public int BookId { get; set; }
+        [Display(Name = "Yorumun Yapıldığı Kitap")]
         public Book Book { get; set; }
     }
 }
